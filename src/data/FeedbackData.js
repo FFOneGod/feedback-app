9450b638fba2803c76eabfa19a0d512c2710a1e7
Emmanuel M Raimi <raimiemmanuel86@gmail.com>
2022-07-26 12:47:17 +0100

    initial

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,18 @@
+const FeedbackData = [{
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea perspiciatis laboriosam recusandae sunt eaque accusamus labore.'
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea perspiciatis laboriosam recusandae sunt eaque accusamus labore.'
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea perspiciatis laboriosam recusandae sunt eaque accusamus labore.'
+  }
+]
+
+export default FeedbackData
